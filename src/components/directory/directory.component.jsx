d796727ba75fcdeff0aa312776f9dbec127c6495
shivansh01 <shivansh@pragyaam.in>
2020-11-23 15:42:44 +0530

    project initial setup

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
+const Directory = () => {
+  const sections = useSelector(selectDirectorySections);
+  return (
+    <div className="directory-menu">
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
